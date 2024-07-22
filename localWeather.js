7f43065afc60e32d9c5a41b8d067e653251e1654
Paul Garton <paulgarton1984@hotmail.com>
2024-07-22 19:03:49 +0100

    add latitude and longitude to display

--- localWeather.js
+++ localWeather.js
@@ -19,11 +19,14 @@ async function fetchWeatherAPI() {
     );
 
     const weatherData = await weatherResponse.json();
+    console.log(weatherData);
     // Instead of returning all the data to the display data function I just return the data that I need. In this case temperature, description and the icon
     const parsedWeatherData = {
       temperature: Math.round(weatherData.list[0].main.temp),
       description: weatherData.list[0].weather[0].main,
       icon: `https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`,
+      lat: weatherData.city.coord.lat,
+      lon: weatherData.city.coord.lon,
     };
 
     return parsedWeatherData;
@@ -41,12 +44,17 @@ async function displayWeather() {
   const weatherTempIconContainer = document.createElement("div");
   const weatherTemperature = document.createElement("p");
   const weatherDescription = document.createElement("p");
+  const weatherLat = document.createElement("p");
+  const weatherLon = document.createElement("p");
   const weatherIcon = document.createElement("img");
 
   //   The temperature needs to be rounded down using Math.round()
   weatherTemperature.textContent = `Current Temperature: ${weatherData.temperature} C`;
 
   weatherDescription.textContent = `Current weather: ${weatherData.description}`;
+
+  weatherLat.textContent = `Lat: ${weatherData.lat}`;
+  weatherLon.textContent = `Lon: ${weatherData.lon}`;
 
   //   Weather Icon, the mock data uses the icon code form open weather API, in this code I have parsed the icon code from the mock fetch and stored in a variable. I can then reuse this in a URL string to display the specific weather icon
   weatherIcon.src = weatherData.icon;
@@ -69,6 +77,8 @@ async function displayWeather() {
   // Append(add) all elements to weatherContainer and in turn append the container to the body
   weatherContainer.appendChild(weatherTempIconContainer);
   weatherTempIconContainer.appendChild(weatherTemperature);
+  weatherTempIconContainer.appendChild(weatherLat);
+  weatherTempIconContainer.appendChild(weatherLon);
   weatherTempIconContainer.appendChild(weatherIcon);
   weatherContainer.appendChild(weatherDescription);
   document.body.appendChild(weatherContainer);
